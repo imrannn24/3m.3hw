2664a90e61ff68db0404e9933bf00f7dc3bfd4a1
imrannn24 <imran200424@icloud.com>
2023-05-25 12:21:15 +0600

    dz

--- 3m.3hw/ViewController.swift
+++ 3m.3hw/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBAction func LoginButton(_ sender: Any) {
+        openMain()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    func openMain(){
+        let mainVc = storyboard?.instantiateViewController(withIdentifier: "main_vc")
+        
+        navigationController?.pushViewController(mainVc!, animated: true)
+    }
 
 }
 
